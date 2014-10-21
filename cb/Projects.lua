50a1e162e62a6f43e1d6853e307e047c77cc5d21
David Cofer <dcofer@NeuroRoboticTech.com>
2014-10-21 05:03:24 -0500

    Modifications to the project files for linux and windows.

--- cb/Projects.lua
+++ cb/Projects.lua
@@ -4,10 +4,22 @@ solution "CarlSim"
 	
 	project "CarlSim"
 		language "C++"
-		kind     "SharedLib"
-		files  { "../*.h",
-				 "../*.cpp",
-				 "../*.cu"}
+		kind     "StaticLib"
+		files  { "../src/config.h",
+				 "../src/CUDAVersionControl.h",
+				 "../src/errorCode.h",
+				 "../src/gpu.h",
+				 "../src/gpu_random.cu",
+				 "../src/gpu_random.h",
+				 "../src/mtrand.cpp",
+				 "../src/mtrand.h",
+				 "../src/printSNNInfo.cpp",
+				 "../src/PropagatedSpikeBuffer.cpp",
+				 "../src/PropagatedSpikeBuffer.h",
+				 "../src/snn.h",
+				 "../src/snn_cpu.cpp",
+				 "../src/snn_gpu.cu",
+				 "../src/v1ColorME.2.1.cu"}
 		includedirs { "/usr/local/cuda/include", 
 					  "/home/ubuntu/NVIDIA_CUDA-6.0_Samples/common/inc" }
 		links { "dl", "cudart" }
